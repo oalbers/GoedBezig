93ac25970a6360d388b85c596be3c045f0410f28
oalbers <olgerik.albers@gmail.com>
2014-10-03 20:24:10 +0200

    Eerste upload

---
+++ app/src/main/java/nl/woonstadrotterdam/ia/goedbezig/MyGoedBezig.java
@@ -0,0 +1,18 @@
+package nl.woonstadrotterdam.ia.goedbezig;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+public class MyGoedBezig extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_goed_bezig);
+    }
+
+
+
+}
